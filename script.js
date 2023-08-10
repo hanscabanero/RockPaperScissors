924315504e88fb14a9f89798fe61ede995a99ac3
hansc <hanscabanero@gmail.com>
2023-08-10 16:41:41 -0600

    Added working playRound function and game function
    completed game and playRound functions respectively, added scaffolding
    for counting score

--- script.js
+++ script.js
@@ -1,24 +1,43 @@
+game();
+//makes a random choice between rock, paper or scissors
+//retuns the choice
 function getComputerChoice() {
-
-    let computerChoice;
-    computerChoice = Math.floor(Math.random() * 3 + 1);
-    const choices = ['Rock','Paper','Scissors'];
-    switch(computerChoice)
-    {
-        case 1:
-            return choices[1];
-        case 2:
-            return choices[1];
-        case 3:
-            return choices[1];
-        default:
-            return console.log("Error");
-    }
+  const choices = ["rock", "paper", "scissors"];
+  const random = Math.floor(Math.random() * choices.length);
+  return choices[random];
 }
-function playRound(playerSelection, computerSelection) {
-    //if player chooses rock & computer chooses paper
+//runs game 5 times, keeps score and declares a winner at the end
+function game() {
+  let playerScore = 0;
+  let computerScore = 0;
+  for(let i = 0; i < 5; i++)
+  {
+    console.log(playRound(playerScore,computerScore));
+    console.log(` ${playerScore} - ${computerScore}`);
   }
-   
-  const playerSelection = "rock";
-  const computerSelection = getComputerChoice();
-  console.log(playRound(playerSelection, computerSelection));
+  console.log(`The final score is player: ${playerScore} wins and computer score:  ${computerScore} wins`)
+}
+
+function playRound(playerScore, computerScore) {
+  //player prompted choice for the round
+    let computerSelection = getComputerChoice();
+    console.log(computerSelection);
+    let playerSelection = prompt("Choose rock, paper, or scissors");
+    //tie case
+    if(playerSelection === computerSelection){
+      return` its a tie! you both chose ${[playerSelection]}`;
+    }
+    //win conditions
+    else if (
+      (playerSelection === 'rock' && computerSelection === 'scissors') ||
+      (playerSelection === 'paper' && computerSelection === 'rock') ||
+      (playerSelection === 'scissors' && computerSelection === 'paper')
+    ) {
+      playerScore++;
+      return `You win! ${playerSelection} beats ${computerSelection}.`;
+      //lose condition
+    } else {
+      computerScore++;
+      return `You lose! ${computerSelection} beats ${playerSelection}.`;
+    }
+  }
