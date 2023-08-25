14e1c4922315e0c0d7a6b430dab746bbbfb51c6f
hansc <hanscabanero@gmail.com>
2023-08-24 18:34:42 -0600

    Completed Final Touches to App
    Finished styling, script and html all together

--- script.js
+++ script.js
@@ -1,43 +1,43 @@
-game();
-//makes a random choice between rock, paper or scissors
-//retuns the choice
-function getComputerChoice() {
-  const choices = ["rock", "paper", "scissors"];
-  const random = Math.floor(Math.random() * choices.length);
-  return choices[random];
-}
-//runs game 5 times, keeps score and declares a winner at the end
-function game() {
-  let playerScore = 0;
-  let computerScore = 0;
-  for(let i = 0; i < 5; i++)
-  {
-    console.log(playRound(playerScore,computerScore));
-    console.log(` ${playerScore} - ${computerScore}`);
-  }
-  console.log(`The final score is player: ${playerScore} wins and computer score:  ${computerScore} wins`)
+const choices = document.querySelectorAll('.choice');
+const scoreElement = document.getElementById('score');
+const resultElement = document.getElementById('result');
+
+let playerScore = 0;
+let computerScore = 0;
+
+choices.forEach(choice => {
+  choice.addEventListener('click', () => {
+    const playerChoice = choice.getAttribute('data-choice');
+    const computerChoice = computerChoose();
+
+    const result = determineWinner(playerChoice, computerChoice);
+    displayResult(result, playerChoice, computerChoice);
+  });
+});
+
+function computerChoose() {
+  const choices = ['Charmander', 'Squirtle', 'Bulbasaur'];
+  const randomIndex = Math.floor(Math.random() * choices.length);
+  return choices[randomIndex];
 }
 
-function playRound(playerScore, computerScore) {
-  //player prompted choice for the round
-    let computerSelection = getComputerChoice();
-    console.log(computerSelection);
-    let playerSelection = prompt("Choose rock, paper, or scissors");
-    //tie case
-    if(playerSelection === computerSelection){
-      return` its a tie! you both chose ${[playerSelection]}`;
-    }
-    //win conditions
-    else if (
-      (playerSelection === 'rock' && computerSelection === 'scissors') ||
-      (playerSelection === 'paper' && computerSelection === 'rock') ||
-      (playerSelection === 'scissors' && computerSelection === 'paper')
-    ) {
-      playerScore++;
-      return `You win! ${playerSelection} beats ${computerSelection}.`;
-      //lose condition
-    } else {
-      computerScore++;
-      return `You lose! ${computerSelection} beats ${playerSelection}.`;
-    }
+function determineWinner(playerChoice, computerChoice) {
+  if (playerChoice === computerChoice) {
+    return 'It\'s a tie!';
+  } else if (
+    (playerChoice === 'Charmander' && computerChoice === 'Bulbasaur') ||
+    (playerChoice === 'Squirtle' && computerChoice ==='Charmander') ||
+    (playerChoice === 'Bulbasaur' && computerChoice === 'Squirtle')
+  ) {
+    playerScore++;
+    return 'You win!';
+  } else {
+    computerScore++;
+    return 'Gary wins!';
   }
+}
+
+function displayResult(result, playerChoice, computerChoice) {
+  scoreElement.textContent = `Player: ${playerScore} - Gary: ${computerScore}`;
+  resultElement.textContent = `${result} You chose ${playerChoice}, Gary  chose ${computerChoice}.`;
+}
